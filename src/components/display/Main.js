d1354c4c2889e7e0162f7c7229d044ce37037c24
richard <r.douglas.42092@gmail.com>
2024-04-17 15:26:40 -0400

    resolving CORS problem and such. yuck.

--- src/components/display/Main.js
+++ src/components/display/Main.js
@@ -104,7 +104,7 @@ const Main = () => {
                 throw new Error(`HTTP error sending data to server! \n **************************\nstatus: ${response.status}`)
             }
             console.log(response)
-            let data = response.json()
+            let data = await response
             console.log("data: ", data)
             
         } catch (error) {

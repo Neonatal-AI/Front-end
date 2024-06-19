e82fe3d7692324089aff40fc49f98f5f07c46133
richard <r.douglas.42092@gmail.com>
2024-06-19 15:05:39 -0400

    minor css changes. disable button onClick.

--- src/components/display/Main.js
+++ src/components/display/Main.js
@@ -215,7 +215,7 @@ const Main = () => {
                     // loading={loginLoading} // I need loginLoading back. it only looked like it didn't do anything
                     
                     onClick={async ()=>{
-                        loading = true;
+                        GradientButton.loading = true;
                         GradientButton.disabled = true;
                         let consult = await Promise.allSettled([documentRequest()]);
                         console.log(consult[0].value)

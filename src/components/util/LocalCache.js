c828213666083e1fa48ba52f55bb803615954e38
richard <r.douglas.42092@gmail.com>
2024-09-21 17:39:15 -0400

    util folder tracked

---
+++ src/components/util/LocalCache.js
@@ -0,0 +1,28 @@
+import {  useState } from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStored] = useState(() => {
+    try {
+      let item = window.localStorage.getItem(key)
+      return item
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    if (value == null){
+        setStored("blank")
+    }
+    try {
+      window.localStorage.setItem(key, value);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
+
+export {useLocalStorage}

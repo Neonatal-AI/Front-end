860c91771bb58504c4ac0d33044facb5f02b1418
richard <r.douglas.42092@gmail.com>
2024-10-04 15:28:07 -0400

    dropdowns collaps when outside click detected

--- src/components/UI/common/DropdownMenu.jsx
+++ src/components/UI/common/DropdownMenu.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const DropdownMenu = ({ label, options, onSelect }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState(null);
+  const dropdownRef = useRef(null);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -20,8 +21,23 @@ const DropdownMenu = ({ label, options, onSelect }) => {
       setIsOpen(!isOpen); // Close the dropdown after selecting an option
     };
 
+    // Close the dropdown if a click is detected outside of it
+    useEffect(() => {
+      const handleOutsideClick = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+          setIsOpen(false);
+        }
+      };
+  
+      document.addEventListener('click', handleOutsideClick);
+  
+      return () => {
+        document.removeEventListener('click', handleOutsideClick);
+      };
+    }, []);
+  
   return (
-    <div className="dropdown" onClick={toggleDropdown} role="button" tabIndex="0" aria-haspopup="true" aria-expanded={isOpen}>
+    <div className="dropdown" ref={dropdownRef} onClick={toggleDropdown} role="button" tabIndex="0" aria-haspopup="true" aria-expanded={isOpen}>
       <span className="dropdown-header" >
         {selectedOption || label || 'Select'}
       </span>
